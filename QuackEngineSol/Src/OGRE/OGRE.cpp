8fc1e984ab9ce9514c046f66f9ff499480277ec7
Jorge <jorgbell@ucm.es>
2021-03-22 19:43:44 +0000

    La ventana se crea

--- QuackEngineSol/Src/OGRE/OGRE.cpp
+++ QuackEngineSol/Src/OGRE/OGRE.cpp
@@ -5,10 +5,19 @@ void OGRE_Init()
 {
 	std::cout << "\n\n\nEMPIEZA LA INICIALIZACIÓN DE OGRE\n\n";
 
+	//paso 1 -> crear root
 	Ogre::Root* root;
 	root = new Ogre::Root();
-
-	/*root->initialise(false);
-	Ogre::SceneManager* scnMgr = root->createSceneManager();
+	//paso 2 -> crear renderwindow
+	Ogre::RenderWindow* mWindow;
+	//paso 3 -> configurar rendersystem
+	Ogre::RenderSystem* rs = root->getRenderSystemByName("Direct3D11 Rendering Subsystem");
+	root->setRenderSystem(rs);
+	rs->setConfigOption("Full Screen", "No");
+	rs->setConfigOption("Video Mode", "800 x 600 @ 32-bit colour");
+	//paso 4-> magic
+	mWindow = root->initialise(true, "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+	
+	/*Ogre::SceneManager* scnMgr = root->createSceneManager();
 	Ogre::Camera* cam = scnMgr->createCamera("myCam");*/
 }
