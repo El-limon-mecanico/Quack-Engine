629f96ea8c3aa7d7cf46334af1bd6e881365b855
Andres <47799692+AndresRuizB@users.noreply.github.com>
2021-04-05 17:54:06 +0200

    Arreglado problema compilacion

--- QuackEngineSol/Projects/LuaBridge/LuaBridgeTest.h
+++ QuackEngineSol/Projects/LuaBridge/LuaBridgeTest.h
@@ -10,16 +10,16 @@
 //}
 //
 //using namespace luabridge;
-//int main() {
-//	lua_State* L = luaL_newstate();
-//	luaL_dofile(L, "script.lua");
-//	luaL_openlibs(L);
-//	lua_pcall(L, 0, 0, 0);
-//	LuaRef s = getGlobal(L, "testString");
-//	LuaRef n = getGlobal(L, "number");
-//	std::string luaString = s.cast<std::string>();
-//	int answer = n.cast<int>();
-//	std::cout << luaString << std::endl;
-//	std::cout << "And here's our number:" << answer << std::endl;
-//
-//}
+int main() {
+	//lua_State* L = luaL_newstate();
+	//luaL_dofile(L, "script.lua");
+	//luaL_openlibs(L);
+	//lua_pcall(L, 0, 0, 0);
+	//LuaRef s = getGlobal(L, "testString");
+	//LuaRef n = getGlobal(L, "number");
+	//std::string luaString = s.cast<std::string>();
+	//int answer = n.cast<int>();
+	//std::cout << luaString << std::endl;
+	//std::cout << "And here's our number:" << answer << std::endl;
+	return 0;
+}
