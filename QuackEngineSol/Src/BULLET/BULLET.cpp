737a25e8e47db6e1aa55957cba4b33114c0ecc2f
MetalCarlosR <carlos.romerog99@hotmail.com>
2021-04-07 11:12:55 +0200

    cosas

--- QuackEngineSol/Src/BULLET/BULLET.cpp
+++ QuackEngineSol/Src/BULLET/BULLET.cpp
@@ -1,5 +1,6 @@
 #include "BULLET.h"
 #include "BtOgre.h"
+#include <iostream>
 
 using namespace BtOgre;
 
@@ -14,20 +15,20 @@ void BULLET_Init() {
 
 	mWindow = root->initialise(true, "DIOS SANTO LA VENTANA");
 
-	Ogre::SceneManager* mSceneMngr;
+	Ogre::SceneManager* mSM;
 
-	mSceneMngr = root->createSceneManager();
+	mSM = root->createSceneManager();
 
 	Ogre::Camera* mCamera;
 
-	mCamera = mSceneMngr->createCamera("MainCam");
+	mCamera = mSM->createCamera("MainCam");
 
 	mCamera->setNearClipDistance(1);
 	mCamera->setFarClipDistance(100000);
 	mCamera->setAutoAspectRatio(true);
 
 
-	Ogre::SceneNode* mNodeCamera = mSceneMngr->getRootSceneNode()->createChildSceneNode();
+	Ogre::SceneNode* mNodeCamera = mSM->getRootSceneNode()->createChildSceneNode();
 	mNodeCamera->attachObject(mCamera);
 
 	mNodeCamera->setPosition(0, 0, 0);
@@ -35,21 +36,52 @@ void BULLET_Init() {
 
 	Ogre::Viewport* vp = mWindow->addViewport(mCamera);
 
-	vp->setBackgroundColour(Ogre::ColourValue(0, 0, 0));
+	vp->setBackgroundColour(Ogre::ColourValue(1, 0, 0));
 
 	mCamera->setAspectRatio(
 		Ogre::Real(vp->getActualWidth()) /
 		Ogre::Real(vp->getActualHeight()));
 
-	mSceneMngr->setAmbientLight(Ogre::ColourValue(.5, .5, .5));
+	mSM->setAmbientLight(Ogre::ColourValue(.5, .5, .5));
 
-	DynamicsWorld* _world = new DynamicsWorld(Ogre::Vector3(0, 1, 0));
+	DynamicsWorld* _world = new DynamicsWorld(Ogre::Vector3(0, -5, 0));
+
+	Ogre::Entity* e = mSM->createEntity(Ogre::SceneManager::PrefabType::PT_CUBE);
+
+	Ogre::SceneNode* meNode = mSM->getRootSceneNode()->createChildSceneNode();
+
+	meNode->attachObject(e);
+
+	meNode->setPosition(0, 160, 0);
+	btRigidBody* rb = _world->addRigidBody(1, e, BtOgre::ColliderType::CT_BOX);
 
 
+	Ogre::Entity* plane = mSM->createEntity(Ogre::SceneManager::PrefabType::PT_PLANE);
+
+	Ogre::SceneNode* mplaneNode = mSM->getRootSceneNode()->createChildSceneNode();
+
+	mplaneNode->attachObject(plane);
+
+	btRigidBody* planeRb = _world->addRigidBody(1, plane, BtOgre::ColliderType::CT_BOX);
+	planeRb->setGravity(btVector3(0, 5, 0));
+
+
+	while (true) {
+		std::cout << "CUBO: yPos->" << rb->getCenterOfMassPosition().y() <<
+			" rot: x->" << rb->getCenterOfMassTransform().getRotation().x() << " y->" << rb->getCenterOfMassTransform().getRotation().y()
+			<< " z->" << rb->getCenterOfMassTransform().getRotation().z();
+		std::cout << " --- " << "PLANO: yPos->" << planeRb->getCenterOfMassPosition().y() <<
+			" rot: x->" << planeRb->getCenterOfMassTransform().getRotation().x() << " y->" << planeRb->getCenterOfMassTransform().getRotation().y()
+			<< " z->" << planeRb->getCenterOfMassTransform().getRotation().z() << std::endl;
+		_world->getBtWorld()->stepSimulation(0.0001);
+		root->_fireFrameStarted();
+		root->renderOneFrame();
+		root->_fireFrameEnded();
+	}
 
 	//while (true)
 	//{
-	//	// Ogre::WindowEventUtilities::messagePump();  WindowEventUtilities se ha mudado con el traidor OgreBites y no sabemos como sustituir esta linea
+	//	// OgreBites::WindowEventUtilities::messagePump();  WindowEventUtilities se ha mudado con el traidor OgreBites y no sabemos como sustituir esta linea
 
 	//	if (mWindow->isClosed()) return;
 
