7444540c2af5c68888ec397370ad259fa57d280d
JuanmaSalva <47757003+JuanmaSalva@users.noreply.github.com>
2021-04-07 11:08:55 +0200

    Fmod (#1)
    * plugin de render añadido y ya no sale la consola en release
    
    * coisas
    
    * La ventana se crea
    
    * añadido codigo fuente de FMOD Core y libs
    
    * fmod inicializado y intento de hacer play
    
    * capa de abstracción
    
    * capa de abstracción hecha
    
    * metodo de pausa
    
    Co-authored-by: Jorge <jorgbell@ucm.es>

--- QuackEngineSol/Src/OGRE/OGRE.cpp
+++ QuackEngineSol/Src/OGRE/OGRE.cpp
@@ -1,10 +1,35 @@
 #include <iostream>
 #include <OgreRoot.h>
+#include "FMOD_Quack.h"
 
-void CargarOGRE()
+
+void OGRE_Init()
 {
-	std::cout << "Aqui deberia estar cargando ogre\n";
+	std::cout << "\n\n\nEMPIEZA LA INICIALIZACIÓN DE OGRE\n\n";
 
+	//paso 1 -> crear root
 	Ogre::Root* root;
 	root = new Ogre::Root();
+	//paso 2 -> crear renderwindow
+	Ogre::RenderWindow* mWindow;
+	//paso 3 -> configurar rendersystem
+	Ogre::RenderSystem* rs = root->getRenderSystemByName("Direct3D11 Rendering Subsystem");
+	root->setRenderSystem(rs);
+	rs->setConfigOption("Full Screen", "No");
+	rs->setConfigOption("Video Mode", "800 x 600 @ 32-bit colour");
+	//paso 4-> magic
+	mWindow = root->initialise(true, "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+	
+	/*Ogre::SceneManager* scnMgr = root->createSceneManager();
+	Ogre::Camera* cam = scnMgr->createCamera("myCam");*/
+}
+
+void prueba(fmod_quack* fmod_sound)
+{
+	fmod_sound->createSound(std::string("singing.wav"), "Cantando");
+	fmod_sound->playSound(0, "Cantando", 1);
+	fmod_sound->createDSP(FMOD_DSP_TYPE_ECHO, std::string("Echo"));
+	fmod_sound->addDSP(0, std::string("Echo"));
+	//fmod_sound->pauseChannel(0, true);
+	//fmod_sound->stopChannel(0);
 }
--- QuackEngineSol/Src/QuackEngine/QuackEnginePro.cpp
+++ QuackEngineSol/Src/QuackEngine/QuackEnginePro.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
 #include <fstream>
-#include "FMOD.h"
+#include "FMOD_Quack.h"
 #include "OGRE.h"
 
-int main()
-{
+//para que no salga la consola en el modo release (en las propiedades del proyecto hay que poner que se
+//ejecute como aplicacion window no cmd (en la parte de vinculador))
+#if (defined _DEBUG) || !(defined _WIN32)
+int main() {
+#else
+#include <windows.h>
+int WINAPI
+WinMain(HINSTANCE zHInstance, HINSTANCE prevInstance, LPSTR lpCmdLine, int nCmdShow) {
+#endif
+
 	//esto es una prueba de los recursos
 	std::ifstream f("Assets/fichero.txt");
-	if(f.is_open())
+	if (f.is_open())
 	{
 		std::cout << "El fichero se ha abierto\n";
 		f.close();
@@ -17,8 +25,11 @@ int main()
 		std::cerr << "ERROR: el fichero no se ha abierto\n";
 	}
 
-	CargarFMOD();
-	CargarOGRE();
+	fmod_quack* fmod_sound = new fmod_quack();
+	
+	OGRE_Init();
+	prueba(fmod_sound);
+	while (true);
 
 	return 0;
 }
