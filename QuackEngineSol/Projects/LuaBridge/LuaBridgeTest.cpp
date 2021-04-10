a01c58287ea3f61e4ff3c8342176baa154cf7601
Jorge Moreno Martínez <jorgemomartin@gmail.com>
2021-04-10 19:29:08 +0200

    función para convertir una tabla de lua en una hashtable

--- QuackEngineSol/Projects/LuaBridge/LuaBridgeTest.cpp
+++ QuackEngineSol/Projects/LuaBridge/LuaBridgeTest.cpp
@@ -3,13 +3,35 @@
 
 using namespace luabridge;
 
+lua_State* L;
+
 void CargarLua() {
-	lua_State* l = luaL_newstate();
-	luaL_dofile(l, "Assets/heroe.lua");
-	luaL_openlibs(l);
-	lua_pcall(l, 0, 0, 0);
-	LuaRef s = getGlobal(l, "character");
-	LuaRef n = s.getMetatable();
-	std::string luastring = n.getGlobal(l, "name").cast<std::string>();
-	std::cout << luastring << std::endl;
+	L = luaL_newstate();
+	//luaL_dofile(L, "Assets/heroe.lua");
+	//luaL_openlibs(L);
+	//lua_pcall(L, 0, 0, 0);
+	//LuaRef s = getGlobal(L, "character");
+	//LuaRef n = s.getMetatable();
+	//std::string luastring = n.getGlobal(L, "name").cast<std::string>();
+	//std::cout << luastring << std::endl;
+}
+
+std::unordered_map<std::string, luabridge::LuaRef> getKeyValueMap(const luabridge::LuaRef& table)
+{
+    using namespace luabridge;
+    std::unordered_map<std::string, LuaRef> result;
+    if (table.isNil()) { return result; }
+
+    auto L = table.state();
+    lua_pushglobaltable(L, table); // push table
+
+    lua_pushnil(L);  // push nil, so lua_next removes it from stack and puts (k, v) on stack
+    while (lua_next(L, -2) != 0) // -2, because we have table at -1
+    {
+        result.emplace(LuaRef::fromStack(L, -2), LuaRef::fromStack(L, -1));
+        lua_pop(L, 1); // remove value, keep key for lua_next
+    }
+
+    lua_pop(L, 1); // pop table
+    return result;
 }
