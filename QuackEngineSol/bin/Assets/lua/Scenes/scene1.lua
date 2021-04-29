fe2f69abf6332b294164e92cc676bbaa3844f432
JuanmaSalva <47757003+JuanmaSalva@users.noreply.github.com>
2021-04-29 15:50:58 +0200

    distincción entre entidades iniciales y prefabs

--- QuackEngineSol/bin/Assets/lua/Scenes/scene1.lua
+++ QuackEngineSol/bin/Assets/lua/Scenes/scene1.lua
@@ -1,3 +1,49 @@
 ﻿scene1 = {
     entities = {"entidad1", "entidad2"}
 }
+
+entidad1 = {
+    Components = {"MeshRenderer", "Transform","Rigidbody", "Prueba"},
+
+    MeshRenderer = {
+        Type = "Sphere",
+        Position = {0,150,0}
+    },
+
+    Transform = {
+        Position = {0,300,0},
+        Scale = {1,1,1},
+        Rotation = {0,0,0}
+    },
+
+    Rigidbody = {
+        Type = "Sphere",
+        Mass = 1
+    },
+
+    Prueba = {
+        valor1 = 1,
+        valor2 = 3,
+        valor3 = "Quack"
+    }
+}
+
+entidad2 = {
+    Components = {"MeshRenderer", "Rigidbody"},
+
+    Transform = {
+        Position = {50,500,0},
+        Scale = {1,1,1},
+        Rotation = {0,0,0}
+    },
+
+    MeshRenderer = {
+        Type = "Sphere",
+        Position = {50,500,0}
+    },
+
+    Rigidbody = {
+        Type = "Sphere",
+        Mass = 1
+    }   
+}
