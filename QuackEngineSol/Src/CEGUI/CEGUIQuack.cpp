91a469c2e3c39b325a90e5daa2b9c1e94bbb4ab7
JuanmaSalva <47757003+JuanmaSalva@users.noreply.github.com>
2021-05-11 17:07:40 +0200

    Capa abstracción de cegui

--- QuackEngineSol/Src/CEGUI/CEGUIQuack.cpp
+++ QuackEngineSol/Src/CEGUI/CEGUIQuack.cpp
@@ -4,6 +4,7 @@
 #include <CEGUI/RendererModules/Ogre/Renderer.h>
 #include <OgreRenderWindow.h>
 #include <assert.h>
+#include <iostream>
 //#include <Ogre.h>
 //#include <OgreRenderTarget.h>
 //#include <CEGUI\ScriptModules\Lua\CEGUILua.h>
@@ -25,6 +26,7 @@ void CEGUIQuack::setUp(Ogre::RenderWindow* rWindow)
 {
 	ogreRenderer_ = &CEGUI::OgreRenderer::bootstrapSystem(*rWindow);
 	CEGUI::SchemeManager::getSingleton().createFromFile("TaharezLook.scheme");
+	CEGUI::SchemeManager::getSingleton().createFromFile("WindowsLook.scheme");
 
 	//CEGUI::System::getSingleton().getDefaultGUIContext().setDefaultFont("DejaVuSans-10");
 	CEGUI::System::getSingleton().getDefaultGUIContext().getMouseCursor().setDefaultImage("TaharezLook/MouseArrow");
@@ -33,12 +35,7 @@ void CEGUIQuack::setUp(Ogre::RenderWindow* rWindow)
 	myRoot->setUsingAutoRenderingSurface(true);	
 	CEGUI::System::getSingleton().getDefaultGUIContext().setRootWindow(myRoot);
 
-	CEGUI::Window* myImageWindow = CEGUI::WindowManager::getSingleton().createWindow("TaharezLook/Button", "PrettyWindow");
-	myImageWindow->setPosition(CEGUI::UVector2(CEGUI::UDim(0.5, 0), CEGUI::UDim(0.5, 0)));
-	myImageWindow->setSize(CEGUI::USize(CEGUI::UDim(0, 150), CEGUI::UDim(0, 50)));
-	myImageWindow->setText("Y E S");
-	//myImageWindow->setProperty("Image", "TaharezLook/full_image");
-	CEGUI::System::getSingleton().getDefaultGUIContext().getRootWindow()->addChild(myImageWindow);
+	setUpResources();
 }
 
 void CEGUIQuack::destroy()
@@ -46,9 +43,6 @@ void CEGUIQuack::destroy()
 	CEGUI::System::getSingleton().destroyGUIContext(*context_);
 }
 
-void CEGUIQuack::draw()
-{
-}
 
 void CEGUIQuack::setUpResources()
 {
@@ -58,6 +52,7 @@ void CEGUIQuack::setUpResources()
 	CEGUI::WidgetLookManager::setDefaultResourceGroup("looknfeel");
 	CEGUI::WindowManager::setDefaultResourceGroup("layouts");
 }
+
 
 void CEGUIQuack::loadScheme(std::string filename)
 {
@@ -69,10 +64,45 @@ void CEGUIQuack::setFont(std::string filename)
 	CEGUI::System::getSingleton().getDefaultGUIContext().setDefaultFont(filename);
 }
 
-CEGUI::Window* CEGUIQuack::createWidget(std::string type, std::string name)
+CEGUI::Window* CEGUIQuack::createWidget(std::string type, std::string name, std::pair<float, float> pos, std::pair<float, float> size)
 {
-	return nullptr;
+	CEGUI::Window* myImageWindow = CEGUI::WindowManager::getSingleton().createWindow(type, name);
+	myImageWindow->setPosition(CEGUI::UVector2(CEGUI::UDim(pos.first, 0.0), CEGUI::UDim(pos.second, 0.0)));
+	myImageWindow->setSize(CEGUI::USize(CEGUI::UDim(0.0, size.first), CEGUI::UDim(0.0, size.second)));
+	//myImageWindow->setProperty("Image", "TaharezLook/full_image");
+	CEGUI::System::getSingleton().getDefaultGUIContext().getRootWindow()->addChild(myImageWindow);
+
+	return myImageWindow;
 }
+
+void CEGUIQuack::createButton(std::string name, std::string text, std::pair<float, float> pos, std::pair<float, float> size)
+{
+	CEGUI::Window* newWidget = nullptr;
+	try	{ newWidget = createWidget("WindowsLook/Button", name, pos, size);	}
+	catch (std::exception e) { std::cout << "No se ha podido crear el boton: " << name << "\n"; }
+	
+	newWidget->setText(text);
+}
+
+void CEGUIQuack::createText(std::string name, std::string text, std::pair<float, float> pos, std::pair<float, float> size)
+{
+	CEGUI::Window* newWidget = nullptr;
+	try { newWidget = createWidget("WindowsLook/StaticText", name, pos, size); }
+	catch (std::exception e) { std::cout << "No se ha podido crear el texto: " << name << "\n"; }
+
+	newWidget->setProperty("Text", text);
+}
+
+void CEGUIQuack::createImage(std::string name, std::string image, std::pair<float, float> pos, std::pair<float, float> size)
+{
+	CEGUI::Window* newWidget = nullptr;
+	try { newWidget = createWidget("WindowsLook/StaticImage", name, pos, size); }
+	catch (std::exception e) { std::cout << "No se ha podido crear la imagen: " << name << "\n"; }
+
+	CEGUI::ImageManager::getSingleton().addFromImageFile(name, image);
+	newWidget->setProperty("Image", name);
+}
+
 
 bool CEGUIQuack::render(double deltaTime)
 {
