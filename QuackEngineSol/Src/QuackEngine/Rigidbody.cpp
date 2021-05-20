cd65b7bed6d4cbc007a4525a5e1c6e11e1f89f63
Tatiana <tatiandu@ucm.es>
2021-05-20 21:32:26 +0200

    Metodos angularVelocity y setAngularVelocity

--- QuackEngineSol/Src/QuackEngine/Rigidbody.cpp
+++ QuackEngineSol/Src/QuackEngine/Rigidbody.cpp
@@ -208,6 +208,16 @@ void Rigidbody::setVelocity(Vector3D v)
 	rb_->setLinearVelocity(v.toBulletPosition());
 }
 
+Vector3D Rigidbody::angularVelocity()
+{
+	return Vector3D::fromBulletPosition(rb_->getAngularVelocity());
+}
+
+void Rigidbody::setAngularVelocity(Vector3D v)
+{
+	rb_->setAngularVelocity(v.toBulletPosition());
+}
+
 void Rigidbody::setPositionConstrains(bool x, bool y, bool z)
 {
 	positionConstrains_ = Vector3D(x, y, z);
@@ -226,7 +236,6 @@ void Rigidbody::removeCollisionData(Rigidbody* rb)
 			it++;
 	}
 }
-
 
 void Rigidbody::setRotationConstrains(bool x, bool y, bool z)
 {
